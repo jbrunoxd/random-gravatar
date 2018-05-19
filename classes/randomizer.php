b33507d18f0b3cd5a881043d71cfc0549467ecb5
Bruno Hamann <jbrunoxd@gmail.com>
2018-05-19 19:07:13 -0300

    update to accept n+1 emails

--- classes/randomizer.php
+++ classes/randomizer.php
@@ -1,23 +1,25 @@
 <?php
 	include('GravatarRPC.class.php');
 	include('octodex.php');
+	include('secrets.php');
 	
 	class Randomizer {
 		public function randomizeGravatar() {
-			// Let's keep our API classes here...
-			$gravatarAPI = new GravatarRPC(YOUR_AKISMET_API_KEY, YOUR_GRAVATAR_EMAIL);
 			$octodexAPI = new Octodex();
-			
-			foreach ($this->convertToArray($gravatarAPI->userimages()) as $gravatar) {
-				// WARNING: this deletes every Gravatar on your account!
-				$gravatarAPI->deleteUserimage(str_replace('.jpg', '', end(explode('/', $gravatar['url']))));
-			}
-			
-			$gravatarURL = $octodexAPI->randomOctocat();
-			$gravatarURL = $gravatarURL['image'];
-			
-			$newGravatar = $gravatarAPI->saveUrl($gravatarURL, 0);
-			$gravatarAPI->useUserimage($newGravatar, YOUR_GRAVATAR_EMAIL);
+			foreach (GRAVATAR_EMAILS as $email) {
+				$gravatarAPI = new GravatarRPC(AKISMET_API_KEY, $email);
+
+				foreach ($this->convertToArray($gravatarAPI->userimages()) as $gravatar) {
+					// WARNING: this deletes every Gravatar on your account!
+					$gravatarAPI->deleteUserimage(str_replace('.jpg', '', end(explode('/', $gravatar['url']))));
+				}
+
+				$gravatarURL = $octodexAPI->randomOctocat();
+				$gravatarURL = $gravatarURL['image'];
+
+				$newGravatar = $gravatarAPI->saveUrl($gravatarURL, 0);
+				$gravatarAPI->useUserimage($newGravatar, $email);	
+			}			
 		}
 		
 		protected function convertToArray($object) {
