32397fb8e2aca7ae2695d788b9532cd370da9981
Bruno Hamann <jbrunoxd@gmail.com>
2018-05-19 19:56:34 -0300

    Update randomizer.php

--- classes/randomizer.php
+++ classes/randomizer.php
@@ -6,7 +6,7 @@
 	class Randomizer {
 		public function randomizeGravatar() {				
 			$max = count($avatars);
-			$gravatarURL = $avatars[random_int(0,$max)]	
+			$gravatarURL = $avatars[random_int(0,$max)];
 			foreach (GRAVATAR_EMAILS as $email) {
 				$gravatarAPI = new GravatarRPC(AKISMET_API_KEY, $email);
 				foreach ($this->convertToArray($gravatarAPI->userimages()) as $gravatar) {
