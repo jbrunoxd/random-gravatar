a2684bc03203ccc0ef68d68fc3af350b4d79df04
Bruno Hamann <jbrunoxd@gmail.com>
2018-05-19 20:05:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
-	include('classes/randomizer.php');
-	
-	$randomizer = new Randomizer();
-	
+	include('classes/randomizer.php');	
+	$randomizer = new Randomizer();	
 	$randomizer->randomizeGravatar();
-
 	echo "Heyo, if all went well, Bruno's gravatar have been changed!";
 ?>
